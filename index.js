ed1bc2309f634143ed5397994ad0e34baabaa19d
Shobhit Chittora <chittorashobhit@gmail.com>
2018-08-07 21:07:37 +0530

    crude script to get all js file

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
+function goToDirAndExec(command) {
+  return `cd ~/Desktop/node && ${command}`;
+}
+
 async function getCommits() {
-  const firstCommit = await exec('cd ~/Desktop/node && git rev-parse HEAD');
-  const lastCommit = await exec('cd ~/Desktop/node && git rev-list --max-parents=0 HEAD');
-  console.log('firstCommit -> ', firstCommit.stdout);
-  console.log('lastCommit -> ', lastCommit.stdout);
+  await exec(goToDirAndExec(`git checkout master`));
+
+  const { stdout: firstCommit } = await exec(goToDirAndExec('git rev-list --max-parents=0 HEAD'));
+  const { stdout: lastCommit } = await exec(goToDirAndExec('git rev-parse HEAD'));
+  
+  console.log(`first commit -> ${firstCommit}, last commit -> ${lastCommit}`);
+  await exec(goToDirAndExec(`git checkout ${lastCommit}`));
+  const { stdout: jsFilesNow } = await exec(goToDirAndExec('find . -type f -name "*.js" | wc -l'));
+
+
+  await exec(goToDirAndExec(`git checkout master`));
+
+  await exec(goToDirAndExec(`git checkout ${firstCommit}`));
+  const { stdout: jsFilesThen } = await exec(goToDirAndExec('find . -type f -name "*.js" | wc -l'));
+
+  console.log(`Then -> ${jsFilesThen}, Now -> ${jsFilesNow}`);
 }
 
 getCommits();
-
-
